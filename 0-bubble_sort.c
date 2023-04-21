6749ca876047c950eb599a37bfcba8cf2b9b6ffb
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-21 08:10:42 +0800

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,26 @@
+#include"sort.h"
+/**
+*bubble_sort-function that sorts an array of integers in Bubble sort(ascending)
+*@array:array to be sorted
+*@size:size of an array
+**/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	int tmp;
 
+	for (; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (j + 1 < size && array[j] > array[j+1])
+			{
+				tmp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = tmp;
+				print_array(array, size);
+				
+			}
+		}
+	}
+}		
