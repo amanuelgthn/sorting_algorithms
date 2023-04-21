e240aa88147161dac57cd4e83ecdc36f0a564572
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-21 08:47:17 +0800

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,11 +13,11 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (j + 1 < size && array[j] > array[j+1])
+			if (j + 1 < size && array[j] > array[j + 1])
 			{
 				tmp = array[j];
-				array[j] = array[j+1];
-				array[j+1] = tmp;
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				print_array(array, size);
 				
 			}
