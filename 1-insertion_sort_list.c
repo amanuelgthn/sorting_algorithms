1f49f84fb69390dece812c576d29965a1d75a9c3
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-23 10:39:36 +0800

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,44 +1,20 @@
 #include"sort.h"
 /**
-*exchange_nodes-exchange consecutive nodes if the later node has a higher data value
-*@node_high: node with a higher data value
-*@node_low: node with a lower data value
-**/
-void exchange_nodes(listint_t *node1, listint_t *node2)
-{
-	listint_t *tmp;
-
-	tmp = node1->next;
-	node1->next = node2->next;
-	node2->next = tmp;
-	tmp = node1->prev;
-	node1->prev = node2->prev;
-	node2->prev = tmp;
-}
-/**
 *insertion_sort-that sorts a doubly linked list of integers in ascending order
 *list-doubly linked list to be sorted
 **/
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *tmp = *list;
+	listint_t *current = *list, *prev = NULL, *next = NULL;
 
-	while((*list)->next)
+	while(current->next)
 	{
-		(*list) = (*list)->next;
-		if((*list)->n < (*list)->prev->n)
+		next= current->next;
+		if(prev == NULL || current->data <= prev->data)
 		{
-			exchange_nodes(tmp,(*list)->prev);
-			print_list(tmp);
-			while ((*list)->prev)
-			{
-				if ((*list)->n < (*list)->prev->n)
-				{
-					exchange_nodes(tmp,(*list)->prev);
-					print_list(tmp);
-				}
-				(*list) = (*list)->prev;
-			}
+			prev = current;
+			current = next;
+			print(current);
 		}
 	}
 }
