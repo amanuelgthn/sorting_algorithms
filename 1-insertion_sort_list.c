98fde6f8e457acf03b6d4150efd72cfba5af6236
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-23 11:44:26 +0800

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,6 +3,7 @@
 *insertion_sort-that sorts a doubly linked list of integers in ascending order
 *list-doubly linked list to be sorted
 **/
+void insertion_sort_list(listint_t **list)
 {
     if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
