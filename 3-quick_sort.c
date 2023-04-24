2eb9163f8c7c6ab531572b0b98bb9ba9aaabc414
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-25 00:27:38 +0800

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,35 +1,51 @@
 #include"sort.h"
 /**
-*quick_sort-function that sorts an array of integers in ascending order by qucik sort
-*@array:array to be sorted
-*@size:size of the array to be sorted
-**/
-void sort_quick(int *arr, int low, int high)
+ * sort_quick - function that sorts an array of integers in ascending order
+ *              using the Quick sort algorithm.
+ * @array: The array to be sorted
+ * @low: The starting index of the array or subarray to be sorted
+ * @high: The ending index of the array or subarray to be sorted
+ */
+void sort_quick(int *array, int low, int high)
 {
-	int pivot = arr[high];
-	int j = low, tmp, i;
+    if (low < high)
+    {
+        int pivot = array[high];
+        int i = low - 1;
 
-	if (low >= high)
-	{
-		print_array(arr,high);
-		return;
-	}
-	for (i = low; i < high; i++)
-	{
-		if (arr[i] <= pivot)
-		{
-			tmp = arr[j];
-			arr[j] = arr[i];
-			arr[i] = tmp;
-		}
-	}
-	tmp = arr[high];
-	arr[high] = arr[j];
-	arr[j] = tmp;
-	sort_quick(arr,low,j-1);
-	sort_quick(arr,low,j-1);
-}	   
+        for (int j = low; j <= high - 1; j++)
+        {
+            if (array[j] <= pivot)
+            {
+                i++;
+                int tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+                print_array(array, high + 1);
+            }
+        }
+
+        int tmp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = tmp;
+        print_array(array, high + 1);
+
+        int pi = i + 1;
+
+        sort_quick(array, low, pi - 1);
+        sort_quick(array, pi + 1, high);
+    }
+}
+/**
+ * quick_sort - function that sorts an array of integers in ascending order
+ *             using the Quick sort algorithm.
+ * @array: The array to be sorted
+ * @size: The size of the array to be sorted
+ */
 void quick_sort(int *array, size_t size)
 {
-	sort_quick(array, 0, size);
+    if (array == NULL || size < 2)
+        return;
+
+    sort_quick(array, 0, size - 1);
 }
