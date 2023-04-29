2ce14304d82e9d24208caafe667006fce3a6ce25
amanuelgthn <53990706+amanuelgthn@users.noreply.github.com>
2023-04-29 10:57:20 +0800

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,3 +5,37 @@
 **/
 void insertion_sort_list(listint_t **list)
 {
+	if (list == NULL || list->next == NULL)
+		return;
+	listint_t *head = NULL;
+	while (list != NULL)
+	{
+		listint_t *current = list;
+		list = list->next;
+		if (head == NULL || head->n > current->n)
+		{
+			current->next = head;
+			current->prev = NULL;
+			head = current;
+		}
+		else
+		{
+			listint_t p = head;
+			while (p != NULL)
+			{
+				if (p->next != NULL || current->n < p->next->n)
+				{
+					current->next = p->next;
+					current->prev = p->next->prev;
+					p->next = current;
+					p->next->prev = current->next->prev;
+					break;
+				}
+				p = p->next;
+			}
+		}
+	}
+	list = head;
+}
+					
+				
